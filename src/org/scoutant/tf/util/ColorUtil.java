9a4829339d53c5532c18add34fa38e75a0436238
coutant <home.coutant@wanadoo.fr>
2011-09-06 11:38:47 +0200

    Fix color when traffic jam

--- src/org/scoutant/tf/util/ColorUtil.java
+++ src/org/scoutant/tf/util/ColorUtil.java
@@ -7,7 +7,11 @@ public class ColorUtil {
 	public static int color(int bfColor) {
 		switch (bfColor) {
 		case -16711936: return Color.rgb(51, 187, 0); 
-		case -26368: return Color.rgb( 255, 212, 38);
+		case -26368: return Color.rgb( 255, 212, 38); // Orange
+//		case -6750208: return Color.rgb( 158, 11, 11); // Red used by Google
+		// Two color for Red? -6750208 and -16776961
+		case -6750208: return Color.RED;
+		case -16776961: return Color.RED;
 		// Gray stands for unknow at Bison Futé. extrapolate to last know?
 		case -9407614: return Color.WHITE; 
 		default:
